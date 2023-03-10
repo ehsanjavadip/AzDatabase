3ef425c5a8c4a7fcb5ea21e26016d3691ff8a630
Ehsan Javadi <54818769+ehsanjavadip@users.noreply.github.com>
2023-03-10 23:23:47 +0330

    Add files via upload

---
+++ تمرین اول/Insert Contactus.sql
@@ -0,0 +1,12 @@
+/*Taklif 1 : Ehsan Javadi
+Ostad : Imani 
+Date Time : 11:22 PM - March 10 (2023)
+SQL Code
+*/
+
+
+/*Contact US Table*/
+
+/*Insert a new Message*/
+INSERT INTO contactus (name,familyname,email,phonenumber,subject,description,attach,emailpre)
+VALUES ("Ehsan","Javadi","ehsanjavadip@gmail.com",09130845536,"Problem in showing my profile informations","this is Description","YES",1);
