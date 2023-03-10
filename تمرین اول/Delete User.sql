aa00b06713ca17c817877f6cc96db63d5e88c628
Ehsan Javadi <54818769+ehsanjavadip@users.noreply.github.com>
2023-03-10 23:29:24 +0330

    Add files via upload

---
+++ تمرین اول/Delete User.sql
@@ -0,0 +1,11 @@
+/*Taklif 1 : Ehsan Javadi
+Ostad : Imani 
+Date Time : 11:22 PM - March 10 (2023)
+SQL Code
+*/
+
+
+/*User Table*/
+
+/*Delete a user :*/
+DELETE FROM users WHERE id = 1;
---
+++ تمرین اول/Insert User.sql
@@ -0,0 +1,12 @@
+/*Taklif 1 : Ehsan Javadi
+Ostad : Imani 
+Date Time : 11:22 PM - March 10 (2023)
+SQL Code
+*/
+
+
+/*User Table*/
+
+/*Insert a new user*/
+INSERT INTO users (name,family,phone,username,password,age,status,job)
+VALUES ("Mehdi","Shariatinia",09224578655,"mahdish","12345678",20,"Active","Flutter Developer");
