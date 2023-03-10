555d482c11a94f209636564be72aca4435ca7802
Ehsan Javadi <54818769+ehsanjavadip@users.noreply.github.com>
2023-03-10 23:27:09 +0330

    Add files via upload

---
+++ تمرین اول/Delete Contactus.sql
@@ -0,0 +1,11 @@
+/*Taklif 1 : Ehsan Javadi
+Ostad : Imani 
+Date Time : 11:22 PM - March 10 (2023)
+SQL Code
+*/
+
+
+/*Contact US Table*/
+
+/*Delete a Message :*/
+DELETE FROM contactus WHERE id = 2;
