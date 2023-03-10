334326f37135a57020fbd34a3b1beac40ca9b026
Ehsan Javadi <54818769+ehsanjavadip@users.noreply.github.com>
2023-03-10 23:37:12 +0330

    Add files via upload

---
+++ تمرین دوم - طراحی جدول/Soft Delete Contactus.sql
@@ -0,0 +1,14 @@
+/*Taklif 1 : Ehsan Javadi
+Ostad : Imani 
+Date Time : 11:22 PM - March 10 (2023)
+SQL Code
+*/
+
+
+/*Contact US Table*/
+
+/*Delete a Message :*/
+/*Update a Message :*/
+UPDATE contactus
+SET softDelete = "Deleted"
+WHERE id = 2;
---
+++ تمرین دوم - طراحی جدول/Soft Delete User.sql
@@ -0,0 +1,14 @@
+/*Taklif 1 : Ehsan Javadi
+Ostad : Imani 
+Date Time : 11:22 PM - March 10 (2023)
+SQL Code
+*/
+
+
+/*User Table*/
+
+/*Delete a user :*/
+/*Update a user :*/
+UPDATE users
+SET softDelete = "Deleted"
+WHERE id = 2;
